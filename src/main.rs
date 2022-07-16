fa642f05d61e88242c687486b30f60f07b8331c1
LuckyTurtleDev <git@lukas1818.de>
2022-07-16 20:07:59 +0200

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 #![warn(rust_2018_idioms)]
 #![forbid(unsafe_code)]
 
-#[macro_use] extern crate rocket;
-use rocket_dyn_templates::Template;
-use rocket_dyn_templates::context;
+#[macro_use]
+extern crate rocket;
+use rocket_dyn_templates::{context, Template};
 
 const CARGO_PKG_NAME: &'static str = env!("CARGO_PKG_NAME");
 const CARGO_PKG_VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 #[get("/")]
 fn index() -> Template {
-	Template::render("index", context! {cargo_pkg_version: CARGO_PKG_VERSION, cargo_pkg_name: CARGO_PKG_NAME})
+	Template::render(
+		"index",
+		context! {cargo_pkg_version: CARGO_PKG_VERSION, cargo_pkg_name: CARGO_PKG_NAME},
+	)
 }
 
 #[launch]
