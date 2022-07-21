f2e80521d78f67cc537c755fe6b7ab540f9c4b31
LuckyTurtleDev <git@lukas1818.de>
2022-07-21 14:57:23 +0200

    improve logging

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,24 @@
 
 #[macro_use]
 extern crate rocket;
-use rocket::http::Status;
-use rocket_dyn_templates::{context, Template};
-mod style;
 use anyhow::{Context, Result};
-use colored::*;
 use dotenv::dotenv;
 use futures_util::future::join_all;
 use mstickereditor::stickerpicker::StickerPack;
 use once_cell::sync::Lazy;
-use rocket::{config::LogLevel, tokio::task::spawn_blocking};
+use rocket::{http::Status, tokio::task::spawn_blocking};
+use rocket_dyn_templates::{context, Template};
 use s3::{Bucket, Region};
-use std::{collections::BTreeMap, env};
+use std::env;
+
+mod style;
 use style::{Style, Theme};
+
+mod logging;
+use logging::{force_init, log_error};
 
 const CARGO_PKG_NAME: &'static str = env!("CARGO_PKG_NAME");
 const CARGO_PKG_VERSION: &'static str = env!("CARGO_PKG_VERSION");
-
-static LOG_LEVEL: Lazy<LogLevel> = Lazy::new(|| {
-	rocket::Config::figment()
-		.extract_inner("log_level")
-		.expect("failed to get log level")
-});
 
 static BUCKET: Lazy<Bucket> = Lazy::new(|| {
 	let s3_server = env::var("PACKS_S3_SERVER").expect("PACKS_S3_SERVER must be set");
@@ -47,9 +43,7 @@ impl<T> ToResultStatus<T> for anyhow::Result<T> {
 		match self {
 			Ok(value) => Ok(value),
 			Err(err) => {
-				if *LOG_LEVEL != LogLevel::Off {
-					eprintln!("   {} {}", ">>".bold(), format!("{} {}", "Error:".bold(), err).red());
-				}
+				log_error(err);
 				Err(Status::InternalServerError)
 			},
 		}
@@ -85,11 +79,11 @@ async fn stickerpicker(user: &str, style: &Style) -> Result<Template> {
 		let mut packs: Vec<StickerPack> = Vec::with_capacity(files.len());
 		for file in files {
 			match file {
-				Err(err) => eprintln!("Error loading Stickerpack from bucket {err}"),
+				Err(err) => log_error(format!("Error loading Stickerpack from bucket {err}")),
 				Ok(value) => {
 					let result: Result<StickerPack, _> = serde_json::from_slice(&value.0);
 					match result {
-						Err(err) => eprintln!("Error parsing Stickerpack {err}"),
+						Err(err) => log_error(format!("Error parsing Stickerpack {err}")),
 						Ok(value) => packs.push(value),
 					}
 				},
@@ -105,9 +99,7 @@ async fn stickerpicker(user: &str, style: &Style) -> Result<Template> {
 #[launch]
 async fn rocket() -> _ {
 	spawn_blocking(|| dotenv()).await.ok();
-	spawn_blocking(|| Lazy::force(&LOG_LEVEL))
-		.await
-		.expect("error getting log level");
+	force_init().await.expect("error getting log level");
 	BUCKET
 		.list("/".to_owned(), Some("/".to_owned()))
 		.await
