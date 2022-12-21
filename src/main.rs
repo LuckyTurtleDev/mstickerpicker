447b3f844ba6e8ca47188a1c8cf8cbc756948a21
LuckyTurtleDev <git@lukas1818.de>
2022-12-21 18:22:54 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,9 @@ static WIDGET_API: Lazy<String> = Lazy::new(|| {
 
 static SQL_POOL: Lazy<sqlx::Pool<sqlx::Postgres>> = Lazy::new(|| {
 	tokio::runtime::Runtime::new().unwrap().block_on(async {
-		sqlx::PgPool::connect("postgres://localhost/mstickerpicker").await.expect("can not connect to database")
+		sqlx::PgPool::connect("postgres://localhost/mstickerpicker")
+			.await
+			.expect("can not connect to database")
 	})
 });
 
